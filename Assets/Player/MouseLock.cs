8d24043930c279ee4e07506f8516b7d87fd894c1
Eric Qian <q110h12@gmail.com>
2016-05-10 20:18:49 -0700

    Added MouseLock script

---
+++ Assets/Player/MouseLock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLock : MonoBehaviour
+{
+    CursorLockMode isLocked;
+
+    // Apply requested cursor state
+    void SetCursorState()
+    {
+        Cursor.lockState = isLocked;
+        // Hide cursor when locking
+        Cursor.visible = (CursorLockMode.Locked != isLocked);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        // Release cursor on escape keypress
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = isLocked = CursorLockMode.None;
+
+        GUILayout.EndVertical();
+
+        SetCursorState();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            isLocked = CursorLockMode.Locked;
+    }
+}
