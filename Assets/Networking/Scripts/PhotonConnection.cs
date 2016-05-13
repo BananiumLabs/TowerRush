a7e87dad6afc841edd6a6bb0627fc77f48e2ab6b
Vikram Peddinti <vikram.peddinti@gmail.com>
2016-05-13 16:49:52 -0700

    Basic Connection Script

---
+++ Assets/Networking/Scripts/PhotonConnection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PhotonConnection : MonoBehaviour {
+    [Header("Photon")]
+    public string versionNumber = "v1.1";
+    public int port = 5055;
+
+    [Header("Connection State")]
+    public Text connectionText;
+
+    private string connectionState = "";
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(versionNumber);
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        connectionText.text = PhotonNetwork.connectionState.ToString();
+    }
+}
