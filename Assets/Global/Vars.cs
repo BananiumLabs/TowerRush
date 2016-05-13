727a4e03937eab0370b1349261d608339ca26e2e
BenCuan <Ben.Cuan@gmail.com>
2016-05-13 16:18:15 -0700

    Reorganized Proton files

---
+++ Assets/Global/Vars.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vars : MonoBehaviour {
+
+	public static int test = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
