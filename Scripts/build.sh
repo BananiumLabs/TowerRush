59883ae58befbccc63ebc7369ca779c9b85387ae
Eric Qian <q110h12@gmail.com>
2016-05-13 19:34:29 -0700

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
- 
-# Name Of Project
-project="unity-game"
- 
+
+project="ci-build"
+
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -12,7 +11,7 @@ echo "Attempting to build $project for Windows"
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
- 
+
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -22,7 +21,7 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
- 
+
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -32,6 +31,6 @@ echo "Attempting to build $project for Linux"
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
- 
+
 echo 'Logs from build'
 cat $(pwd)/unity.log
