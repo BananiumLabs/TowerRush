5b986cd89b8deef493edf3a5665e81a858102ec4
Vikram Peddinti <vikram.peddinti@gmail.com>
2016-05-13 20:36:25 -0700

    New UI PlayerName

--- Assets/Networking/Scripts/PhotonConnection.cs
+++ Assets/Networking/Scripts/PhotonConnection.cs
@@ -10,6 +10,14 @@ public class PhotonConnection : MonoBehaviour {
     [Header("Connection State")]
     public Text connectionText;
 
+    [Header("Player Info")]
+    public Text playerfield;
+    public GameObject playerPanel;
+    private string plname;
+
+    [Header("Menu UI")]
+    public GameObject roomPanel;
+
     private string connectionState = "";
 	// Use this for initialization
 	void Start () {
@@ -20,5 +28,30 @@ public class PhotonConnection : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         connectionText.text = PhotonNetwork.connectionState.ToString();
+    }
+
+
+
+
+
+    public void createRoom()
+    {
+        Debug.Log("Creating Room");
+        PhotonNetwork.CreateRoom("Room 1");
+        //PhotonNetwork.JoinRoom("Room 1");
+        
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.playerName);
+    }
+
+    public void playerFieldname()
+    {
+        PhotonNetwork.playerName = playerfield.text;
+        playerPanel.SetActive(false);
+        roomPanel.SetActive(true);
+
     }
 }
