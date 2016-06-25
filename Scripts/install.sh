163e59c8a9f5849c01ca3cc5678e9ae7575f0fe6
Eric Qian <q110h12@gmail.com>
2016-06-25 14:17:03 -0700

    Update Editor To Beta22

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo 'Downloading from http://netstorage.unity3d.com/unity/fdbb5133b820/MacEditorInstaller/Unity-5.3.4f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/fdbb5133b820/MacEditorInstaller/Unity-5.3.4f1.pkg
+curl -o Unity.pkg http://beta.unity3d.com/download/7633684eb4c7/MacEditorInstaller/Unity-5.4.0b22.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
