38168b942a45c45fcd609b193fd57cce705262cb
Vikram Peddinti <vikram.peddinti@gmail.com>
2016-12-16 20:07:04 -0800

    DarkRift Port Setup
    Okay, I started setting up the DarkRift Port. I plan to have the
    movement and animations working before I leave. The movements WILL NOT
    BE AUTHORITATIVE!

---
+++ Assets/Networking/Scripts/Pl_NetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkRift;
+
+public class Pl_NetworkManager : MonoBehaviour {
+
+    public string IP = "127.0.0.1";
+
+    void Start()
+    {
+        DarkRiftAPI.Connect(IP);
+    }
+
+    void OnApplicationQuit()
+    {
+        DarkRiftAPI.Disconnect();
+    }
+}
