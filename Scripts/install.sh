b2cf40c0ed224957435efad7cf6967acc470d46a
Eric Qian <q110h12@gmail.com>
2016-12-16 17:27:03 -0800

    Travis Build Fix

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
-echo 'Downloading from http://beta.unity3d.com/download/7633684eb4c7/MacEditorInstaller/Unity-5.4.0b22.pkg: '
-curl -o Unity.pkg http://beta.unity3d.com/download/7633684eb4c7/MacEditorInstaller/Unity-5.4.0b22.pkg
+echo 'Downloading from http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorInstaller/Unity-5.5.0f3.pkg: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorInstaller/Unity-5.5.0f3.pkg
 
 
-echo 'Downloading from http://beta.unity3d.com/download/7633684eb4c7/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.4.0b22.pkg'
-curl -o UnityWindowsSupport.pkg http://beta.unity3d.com/download/7633684eb4c7/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.4.0b22.pkg
+echo 'Downloading from http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.0f3.pkg'
+curl -o UnityWindowsSupport.pkg http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.0f3.pkg
 
-echo 'Downloading from http://beta.unity3d.com/download/7633684eb4c7/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-5.4.0b22.pkg'
-curl -o UnityLinuxSupport.pkg http://beta.unity3d.com/download/7633684eb4c7/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-5.4.0b22.pkg
+echo 'Downloading from http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-5.5.0f3.pkg'
+curl -o UnityLinuxSupport.pkg http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-5.5.0f3.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
