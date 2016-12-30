eaf72101ddb05bc0116dbed0d7b33464cf6b3e23
Ben C <ben.cuan@gmail.com>
2016-12-30 13:53:28 -0800

    Fix bug: player drifts away

---
+++ Assets/Scripts/Player/Pl_PositionStabilizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///Fixes bug where player drifts from camera
+public class Pl_PositionStabilizer : MonoBehaviour {
+
+Vector3 position;
+
+	// Sets position to optimal position of player
+	void Start () {
+		position = transform.localPosition;
+	}
+	
+	// If player has drifted, reset its position relative to container prefab
+	void Update () {
+		if(!transform.localPosition.Equals(position))
+			transform.localPosition = position;
+	}
+}
