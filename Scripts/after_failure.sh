68356b8c05c0cf48b10f5003578372930b128875
Eric Qian <q110h12@gmail.com>
2017-01-06 22:55:45 -0800

    Fix rcpt address

--- Scripts/after_failure.sh
+++ Scripts/after_failure.sh
@@ -4,7 +4,7 @@ echo "Build Failed! Sending Notification Message. Please Wait"
 curl -s --user 'api:key-2216f9d7fc3e654fce523dd3d56d21c6' \
     https://api.mailgun.net/v3/m.enumc.com/messages \
     -F from='API Travis <api@m.enumc.com>' \
-    -F to='2TBS Developers <eric@enumc.com>' \
+    -F to='2TBS Developers <admin@fewdpew.me>' \
     -F subject='TowerRush Build Failed - Travis CI' \
     -F text='Travis build failed. This is a notification message. No reply.' 
 echo "Curl request sent!"
