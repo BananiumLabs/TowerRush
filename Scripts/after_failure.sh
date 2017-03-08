d660ea0e40a70c5f520d11b4d9769af102351744
Eric Qian <q110h12@gmail.com>
2017-03-07 21:36:04 -0800

    Fix Log For Failing Builds
    Aryan already broke it. So Travis will still be failing... This helps to debug what is the error.

--- Scripts/after_failure.sh
+++ Scripts/after_failure.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+echo 'Logs from failing build'
+cat $(pwd)/unity.log
+
 echo "Build Failed! Sending Notification Message. Please Wait"
 curl -s --user 'api:key-2216f9d7fc3e654fce523dd3d56d21c6' \
     https://api.mailgun.net/v3/m.enumc.com/messages \
